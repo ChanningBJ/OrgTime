2f18a6c8141b509ad6cd1c4ec61889efa814425d
Chengming <wangchengming.jlu@gmail.com>
2013-09-28 19:29:53 +0800

    insert BEGIN END in org file

---
+++ orgtime.el
@@ -0,0 +1,31 @@
+
+(defun org-time-chart ()
+  "Take a screenshot into a unique-named file in the current buffer file
+  directory and insert a link to this file."
+  (interactive)
+  ;; (setq filename
+  ;;   (concat (make-temp-name
+  ;;        (concat  (getenv "HOME") "/.emacs.img/" ) ) ".png"))
+  ;; (suspend-frame)
+  (setq msg (shell-command-to-string "ls -l"))
+  ;; (call-process-shell-command "scrot" nil nil nil nil " -s " (concat
+  ;;                               "\"" filename "\"" ))
+  (setq summary_content_begin "#+BEGIN: clocktable")
+  (setq summary_content_end "#+END")
+  (beginning-of-buffer)
+  (search-forward summary_content_begin)
+  (beginning-of-line)
+  (setq begin_pos (point))
+  (search-forward summary_content_end)
+  (end-of-line)
+  (setq end_pos (point))
+  (delete-region begin_pos end_pos)
+  (insert (concat summary_content_begin " " (format-time-string "%Y-%m-%d %H-%M-%S\n")))
+;  (insert buffer-file-name msg "]]"))
+  (insert msg)
+  (insert (concat "\n" summary_content_end))
+  )
+ 
+;(global-set-key (kbd "C-p") 'my-screenshot)
+
+  
